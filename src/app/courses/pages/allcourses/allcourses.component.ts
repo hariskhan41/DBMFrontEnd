4808f6680a28827d40e947d8bc26503f233c6ffe
Ayesha <ayeshaatif@gmail.com>
2019-08-18 03:43:28 -0700

    Course Page responsive

--- src/app/courses/pages/allcourses/allcourses.component.ts
+++ src/app/courses/pages/allcourses/allcourses.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-allcourses',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./allcourses.component.css']
 })
 export class AllcoursesComponent implements OnInit {
-
+  ngVersion: string = VERSION.full;
+  matVersion: string = '5.1.0';
+  breakpoint: number;
   constructor() { }
 
   ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 800 ) ? 2 : 4;
+    // this.breakpoint = (window.innerWidth <= 500) ? 1 : 4;
   }
+  
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 800) ? 2 : 4;
+    // this.breakpoint = (event.target.innerWidth <= 500) ? 1 : 4;
+  }
+ 
+  
 
 }
