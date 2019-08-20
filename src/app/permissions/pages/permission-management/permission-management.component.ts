17418e56a8a1f4452886282058ffbfc6d43ccab5
Ayesha <ayeshaatif@gmail.com>
2019-08-19 23:27:27 -0700

    Add Group Page and permission page updation

--- src/app/permissions/pages/permission-management/permission-management.component.ts
+++ src/app/permissions/pages/permission-management/permission-management.component.ts
@@ -1,46 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Component } from '@angular/core';
 
+import {FormControl} from '@angular/forms';
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+ 
+  icons:string;
+  Users:string;
+  permission:string;
+  SelectUser:string;
+  Group:string;
+ 
+}
+export interface Food {
+  value: string;
+  viewValue: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+ {Group:'Admin',icons:'', Users:'', permission:'', SelectUser:''},
+ {Group:'Teacher',icons:'', Users:'', permission:'', SelectUser:''}, 
+ {Group:'Student',icons:'', Users:'', permission:'', SelectUser:''},  
 ];
 @Component({
   selector: 'app-permission-management',
   templateUrl: './permission-management.component.html',
   styleUrls: ['./permission-management.component.css'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
-export class PermissionManagementComponent implements OnInit {
+export class PermissionManagementComponent{
   dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
-
-  expandedElement: PeriodicElement | null;
-  constructor() { }
-
-  ngOnInit() {
-  }
+  displayedColumns :string[]=['icons', 'Group', 'Users', 'SelectUser', 'permission'];
+ 
+  foods: Food[] = [
+    {value: 's0', viewValue: 'User1'},
+    {value: 's1', viewValue: 'User2'},
+    {value: 's2', viewValue: 'User3'}
+  ];
   
-
+  permissions = new FormControl();
+  permissionList: string[] = ['Abc', 'Xyz', 'Klm', 'per1'];
 }
